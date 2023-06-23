1e9da0df35a3c15e358a1f691a9d48da97b2070a
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-06-22 22:32:27 -0600

    Few changes

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template, request
 import requests
 import json
 import pandas as pd
+import sqlite3
 
 app = Flask(__name__)
+conn = sqlite3.connect('./database/animes_vistos.db')
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -21,23 +23,3 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
-
-
-# anime_name = "mashle"
-
-# data = requests.get(f'https://api.jikan.moe/v4/anime?q={anime_name}')
-# results = json.loads(data.text)
-# df = pd.json_normalize(results)
-
-# # df2 = df.data # Éste es un objeto de Pandas
-# # df2 = df.data[0] # Ésta es una lista
-# df2 = df.data[0][0] # Éste es un diccionario
-
-# anime_title = df2['title']
-# anime_airing = df2['airing']
-# anime_NoChapters = df2['episodes']
-
-# if anime_airing == True:
-#     print(f'{anime_title} sigue vigente y tiene {anime_NoChapters} capítulos')
-# else:
-#     print(f'{anime_title} ya terminó y tuvo {anime_NoChapters} capítulos')
