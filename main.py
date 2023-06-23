8540bc11a1a903e60bfd25cc612c11d4d1be8fc9
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-06-23 04:54:39 -0600

    Agregué una clase para poder guardar los datos de los animes que clickee

--- main.py
+++ main.py
@@ -7,6 +7,20 @@ import sqlite3
 app = Flask(__name__)
 conn = sqlite3.connect('./database/animes_vistos.db')
 
+class Anime():
+    def __init__(self, id_anime, name_anime, chap_anime, stat_anime):
+        self.id_anime = id_anime
+        self.name_anime = name_anime
+        self.chap_anime = chap_anime
+        self.stat_anime = stat_anime
+    
+    def anime_info(self):
+        print(self.id_anime)
+        print(self.name_anime)
+        print(self.chap_anime)
+        print(self.stat_anime)
+
+@app.route('/index.html')
 @app.route('/', methods=['POST', 'GET'])
 def index():
     
@@ -20,6 +34,20 @@ def index():
         return render_template('index.html', animes = results['data'])
     else:
         return render_template('index.html')
+    
+@app.route('/anime_list', methods=['GET', 'POST'])
+def anime_list():
+    if request.method == 'GET':
+        return render_template('anime_list.html')
+    
+    if request.method == 'POST':
+        url = "https://api.jikan.moe/v4/anime?q=" + request.form['search']
+        data = requests.get(url)
+        results = json.loads(data.text)
+        animes = results['data']
+    
+        anime = Anime(animes['mal_id'], animes['title'], animes['episodes'], animes['status'])
+        anime.anime_info
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
